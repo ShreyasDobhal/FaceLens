c2b3f8ebd63880a11e680145e4e906deb0619907
ShreyasDobhal <shreyasdobhal@gmail.com>
2020-03-25 19:54:54 +0530

    Changed paths and added Raw folder

--- paths.py
+++ paths.py
@@ -1,6 +1,6 @@
 FACE_CASCADE_XML_FILE_PATH = '/home/shreyas/Desktop/FaceRecog/haarcascade_frontalface_default.xml'
 RAW_IMAGES_FOLDER_PATH = ['/home/shreyas/Desktop/FaceRecog/Raw/']
-FACES_FOLDER_PATH = '/home/shreyas/Desktop/FaceRecog/Amitabh/'
+FACES_FOLDER_PATH = '/home/shreyas/Desktop/FaceRecog/Faces/'
 TEMP_FILE_PATH = '/home/shreyas/Desktop/FaceRecog/temp.jpg'
 DATASET_FOLDER_PATH = '/home/shreyas/Desktop/FaceRecog/Dataset/'
 PICKLED_DATA_FILE_PATH = '/home/shreyas/Desktop/FaceRecog/dataset.pkl'
